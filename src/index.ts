cefaea6c1b3aca98c627757f306d0e804e98bae5
fran <franlobogarcia@gmail.com>
2025-08-05 18:24:52 +0200

    feat: updata package json commands

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express = require("express");
 import dotenv = require("dotenv");
 import type { Express, Request, Response } from "express";
+import mysql = require("mysql2/promise");
 const app: Express = express();
 const envPath =
   process.env.NODE_ENV === "production"
@@ -9,10 +10,23 @@ const envPath =
 dotenv.config({ path: envPath });
 const PORT = process.env.PORT || 3000;
 
+const testDbConnection = async () => {
+  try {
+    const connection = await mysql.createConnection(
+      process.env.DATABASE_URL as string
+    );
+    await connection.end();
+    console.log("databse connection successful");
+  } catch (error) {
+    console.error("Database connection failed: ", error);
+  }
+};
+
 app.get("/", (req, res, next) => {
   res.send("hello fran");
 });
 
-app.listen(PORT, () =>
-  console.log(`[server]: Server is running at http://localhost:${PORT}`)
-);
+app.listen(PORT, () => {
+  console.log(`[server]: Server is running at http://localhost:${PORT}`);
+  testDbConnection();
+});
